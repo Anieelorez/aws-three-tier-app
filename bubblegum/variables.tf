b664d4e09a377092c28caadaa07a7a65a06a86b8
Anieelorez <anieelorez1@gmail.com>
2024-04-19 15:57:10 +0100

    modified variables

--- bubblegum/variables.tf
+++ bubblegum/variables.tf
@@ -1,6 +1,9 @@
 variable "region" {}
 variable "vpc_project_name" {}
 variable "environment" {}
+variable "project_name" {
+  
+}
 
 #vpc  variable
 variable "vpc_cidr" {}
@@ -13,3 +16,7 @@ variable "private_app_subnet_az3_cidr" {}
 variable "private_db_subnet_az1_cidr" {}
 variable "private_db_subnet_az2_cidr" {}
 variable "private_db_subnet_az3_cidr" {}
+
+#acm variable
+variable "domain_name" {}
+variable "subject_alternative_names" {}
