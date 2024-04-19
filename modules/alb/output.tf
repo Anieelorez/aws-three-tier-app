cdcb851110d606af5c7102c373fae439f8799538
Anieelorez <anieelorez1@gmail.com>
2024-04-19 15:17:56 +0100

    created load balancer

---
+++ modules/alb/output.tf
@@ -0,0 +1,12 @@
+output "alb_target_group_arn" {
+  value = aws_lb_target_group.alb_target_group.arn
+}
+
+output "application_load_balancer_dns_name" {
+  value = aws_lb.application_load_balancer_dns_name
+}
+
+output "application_load_balancer_zone_id" {
+    value = aws_lb.application_load_balancer_zone_id
+}
+
---
+++ modules/alb/variables.tf
@@ -0,0 +1,7 @@
+variable "project_name" {}
+variable "alb_security_group_id" {}
+variable "public_subnet_az2_id" {}
+variable "public_subnet_az1_id" {}
+variable "public_subnet_az3_id" {}
+variable "vpc_id" {}
+variable "certificate_arn" {}
