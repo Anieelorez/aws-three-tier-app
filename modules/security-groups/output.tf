9b3c9388a3882874b505eb5b6eb70a33bddec9dc
Anieelorez <anieelorez1@gmail.com>
2024-04-20 12:31:15 +0100

    updated output

--- modules/security-groups/output.tf
+++ modules/security-groups/output.tf
@@ -2,6 +2,6 @@ output "alb_security_group_id" {
     value = aws_security_group.alb_security_group.id
 }
 
-output "ecs_security_group" {
-    value = aws_security_group.ecs_security_group.id
+output "ec2_security_group" {
+    value = aws_security_group.ec2_security_group.id
 }
